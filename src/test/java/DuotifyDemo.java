f6585a04a0bbb810c6072bc4783071ffc65357bf
Mukhayyo <gaffarovamukhayyo1990@gmail.com>
2023-10-13 20:57:11 -0400

    Public

--- src/test/java/DuotifyDemo.java
+++ src/test/java/DuotifyDemo.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import  org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
-
+// new change
 public class DuotifyDemo {
 
     public static void main(String[] args) {
@@ -14,60 +14,81 @@ public class DuotifyDemo {
         WebDriver driver = new ChromeDriver();
         driver.get("http://duotify.us-east-2.elasticbeanstalk.com/register.php");
 
-        String title = driver.getTitle();
-        System.out.println(title);
+        Assert.assertEquals(driver.getTitle(), "Welcome to Duotify!");
 
         WebElement signUpLink = driver.findElement(By.id("hideLogin"));
         signUpLink.click();
 
         Faker faker = new Faker();
-         String userName = faker.name().username();
-       // System.out.println(username);
+        String userName = faker.name().username();
         String firstName = faker.name().firstName();
-        //System.out.println();
         String lastName = faker.name().lastName();
         String email = faker.internet().emailAddress();
-        String confirmEmail = faker.internet().emailAddress();
         String password = faker.internet().password();
-        String confirmPass = faker.internet().password();
+
 
         driver.findElement(By.name("username")).sendKeys(userName);
         driver.findElement(By.name("firstName")).sendKeys(firstName);
         driver.findElement(By.name("lastName")).sendKeys(lastName);
         driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("email2")).sendKeys(confirmEmail);
+        driver.findElement(By.name("email2")).sendKeys(email);
         driver.findElement(By.name("password")).sendKeys(password);
-        driver.findElement(By.name("password2")).sendKeys(confirmPass);
+        driver.findElement(By.name("password2")).sendKeys(password);
+        driver.findElement(By.name("registerButton")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"http://duotify.us-east-2.elasticbeanstalk.com/browse.php?" );
 
-        driver.findElement(By.name("signupButton")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "http://duotify.us-east-2.elasticbeanstalk.com/browse.php?");
+        String fullName = (driver.findElement(By.id("nameFirstAndLast"))).getText();
+        if (fullName.equals(firstName + " " + lastName)) {
+            System.out.println("First and Last name match!");
+        } else {
+            System.out.println("First and Last name do not match.");
+        }
 
-        String navBarName = driver.findElement(By.className("navBar__username")).getText();
-        Assert.assertEquals(navBarName, firstName + " " + lastName);
+        WebElement usernameLink = (driver.findElement(By.id("nameFirstAndLast")));
+        usernameLink.click();
 
-
-        driver.findElement(By.className("navBar__username")).click();
-        String mainUsername = driver.findElement(By.id("username")).getText();
-        Assert.assertEquals(mainUsername, userName);
-
-
-        driver.findElement(By.id("logoutButton")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "http://duotify.us-east-2.elasticbeanstalk.com/register.php");
-
-
+        String actualUsername = (driver.findElement(By.id("nameFirstAndLast"))).getText();
+        if (actualUsername.equals(fullName)) {
+            System.out.println("Username is correct!");
+        } else {
+            System.out.println("Username is incorrect.");
+        }
+        driver.findElement(By.id("rafael")).click();
+        String currentUrl = driver.getCurrentUrl();
+        if (currentUrl.equals("http://duotify.us-east-2.elasticbeanstalk.com/register.php")){
+            System.out.println("URL matches!");
+        } else{
+            System.out.println("URL does not match!");
+        }
         driver.findElement(By.name("loginUsername")).sendKeys(userName);
         driver.findElement(By.name("loginPassword")).sendKeys(password);
         driver.findElement(By.name("loginButton")).click();
 
+        String pageSource = driver.getPageSource();
+        String textToVerify = "You Might Also Like";
+        System.out.println(pageSource);
 
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("You Might Also Like"));
+        if (pageSource.contains(textToVerify)) {
+            System.out.println("Login successful. Found the text: " + textToVerify);
+        } else {
+            System.out.println("Login failed. Could not find the text: " + textToVerify);
+        }
 
-        driver.findElement(By.className("navBar__username")).click();
-        driver.findElement(By.id("logoutButton")).click();
+        WebElement usernameLink2 = (driver.findElement(By.id("nameFirstAndLast")));
+        usernameLink2.click();
+        driver.findElement(By.id("rafael")).click();
+
         Assert.assertEquals(driver.getCurrentUrl(), "http://duotify.us-east-2.elasticbeanstalk.com/register.php");
 
-
         driver.quit();
+
+
+
+
+
+
+
+
 
 
 
